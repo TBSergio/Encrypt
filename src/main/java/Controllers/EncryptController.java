87233e81cd9f696ebddca86ebf8e9bac2e28cc20
TBSergio <TBSergio@walla.com>
2017-03-06 23:16:16 +0200

    First Stage ----------- 1.LogicController -added method to check if a path is correct and keep asking for a path untill it is correct or * is pressed 2.main - added calls to Encrypt and Decrypt

--- src/main/java/Controllers/EncryptController.java
+++ src/main/java/Controllers/EncryptController.java
@@ -1,7 +1,19 @@
 package Controllers;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by Sergei on 3/6/2017.
  */
 public class EncryptController {
+
+    public static void Encrypt(InputStream in) {
+
+    }
+
 }
--- src/main/java/Controllers/LogicController.java
+++ src/main/java/Controllers/LogicController.java
@@ -1,12 +1,21 @@
 package Controllers;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
 
 /**
  * Created by Sergei on 3/6/2017.
  */
 public class LogicController {
 
+    private static boolean Flag = false;
+    private static InputStream in = null;
     public final static void clearConsole() {
         try {
             final String os = System.getProperty("os.name");
@@ -18,4 +27,26 @@ public class LogicController {
             System.out.println("Sorry - Could'nt clear console!\n");
         }
     }
+    public static InputStream checkFilePath()
+    {
+        Scanner input = new Scanner(System.in);
+        Flag = false;
+        while(!Flag) {
+            try {
+                System.out.print("Please Insert Valid File Path for Encryption!(Or '*' To Close)\nFile Path:");
+                String pathString = input.next();
+                System.out.println("----------------------");
+                if(pathString.equals("*"))
+                    exit(0);
+                Path path = Paths.get(pathString);
+                in = Files.newInputStream(path);
+                Flag = true;
+
+            } catch (IOException e) {
+                System.out.println("Invalid Path to File or File Does'nt Exist!");
+            }
+        }
+        return in;
+    }
+
 }
